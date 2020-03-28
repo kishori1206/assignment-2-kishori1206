0cf2062db5906bfc2782877880bec2a6484df573
kishori1206 <61354085+kishori1206@users.noreply.github.com>
2020-03-28 12:55:41 +0530

    Update MyMain.java

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,19 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+
 //executable class
 public class MyMain {
+  MyCircularQueue obj = new MyCircularQueue();
+        obj.enqueue(1,9,0);
+        obj.enqueue(2,8,1);
+        obj.enqueue(3,7,4);
+        obj.enqueue(4,6,1);
+        obj.enqueue(5,6,2);
+        obj.removeBackLog();
+        obj.display();
+
+    }
 }
