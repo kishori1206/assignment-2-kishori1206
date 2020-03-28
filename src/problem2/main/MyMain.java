af56d89cd154dabab5065d3d686d7a8cbd584b72
kishori1206 <61354085+kishori1206@users.noreply.github.com>
2020-03-28 12:40:03 +0530

    Update MyMain.java

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -8,4 +8,14 @@ package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+   public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(50);
+        obj.insert(10);
+        obj.insert(68);
+        obj.insert(90);
+        obj.insert(40);
+        obj.insert(30);
+        obj.preVSpost();
+    }
 }
